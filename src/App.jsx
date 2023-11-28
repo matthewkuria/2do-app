259b37c65530d6fe1e4f5a1780881dc5eeef0b90
MatthewKuria <mathewkwachira@gmail.com>
2023-11-28 23:51:04 +0300

    add unique id props to the component

--- src/App.jsx
+++ src/App.jsx
@@ -51,15 +51,18 @@ function App(props) {
         <Todo  
         name = "Eat"
         completed = {true} 
+        id="todo-0"
         />
         <Todo 
         name="Sleep"
         completed = {false} 
+        id="todo-1"
 
         />
         <Todo 
         name="Repeat"
         completed = {false} 
+        id="todo-2"
 
         />
 
