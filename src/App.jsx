2667af1facfbb47308504f22517cf33cb0114c41
MatthewKuria <mathewkwachira@gmail.com>
2023-11-29 00:12:41 +0300

    refactor the code to have less repetitive components

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ import Todo from "./components/Todo";
 
 
 function App(props) {
+  const taskList = props.tasks?.map(task => <Todo 
+    name={task.name}
+    id ={task.id}
+    completed= {task.completed}
+    key= {task.id} //Help react keep track of tasks
+    />)
   return (
     <div className="todoapp stack-large">
       <h1>To-do-app</h1>
@@ -49,27 +55,7 @@ function App(props) {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
 
-        <Todo  
-        // add props to the TOdo components
-        name = "Eat"
-        completed = {true} 
-        id="todo-0"
-        />
-        <Todo 
-        name="Sleep"
-        completed = {false} 
-        id="todo-1"
-
-        />
-        <Todo 
-        name="Repeat"
-        completed = {false} 
-        id="todo-2"
-
-        />
-
-        
-        
+        {taskList}
       </ul>
     </div>
   );
