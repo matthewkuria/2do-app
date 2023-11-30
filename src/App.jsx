4c969ae6aafc440b0becd2f731cc03194b1aa222
MatthewKuria <mathewkwachira@gmail.com>
2023-11-30 23:09:23 +0300

    Add edit functionality to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { nanoid } from "nanoid";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,13 +7,22 @@ import Form from './components/Form'
 import FilterButton from './components/FilterButton'
 import Todo from "./components/Todo";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
+  const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 
   function addTask(name) {
     // we will make a newTask object to add to the array
-    const newTask = { name, completed: false };
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
     setTasks([...tasks, newTask]);
   }
   function toggleTaskCompleted(id) {
@@ -33,6 +43,18 @@ function App(props) {
     setTasks(remainingTasks);
   }
   
+  // Modify or edit task code
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
   
   
   const taskList = tasks.map(task =>
@@ -43,8 +65,19 @@ function App(props) {
     key= {task.id} //Help react keep track of tasks
     toggleTaskCompleted={toggleTaskCompleted}
     deleteTask={deleteTask}
+    editTask={editTask}
     />
     )
+    const filterList = FILTER_NAMES.map((name) => (
+      <FilterButton
+        key={name}
+        name={name}
+        isPressed={name === filter}
+        setFilter={setFilter}
+      />
+    ));
+    
+    
     //Count the number of tasks added
     const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
     const headingText = `${taskList.length} ${tasksNoun} remaining`;
@@ -54,9 +87,9 @@ function App(props) {
       <h1 className="app--title">To-do-app</h1>
      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+
+      {filterList}
+
 
       </div>
       <h2 id="list-heading">{headingText}</h2>
